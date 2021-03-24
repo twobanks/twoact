270617dc82feddc6170ceb847cd04ac2489b4f2a
twobanks <twobanks@me.com>
2021-03-24 01:55:50 -0300

    add strava api

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,9 +1,74 @@
+import React, { useState, useEffect } from 'react'
 import * as S from './styled'
 
-const Main = () => (
-  <S.Wrapper>
-    <S.Banks />
-  </S.Wrapper>
-)
+const Main = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [activity, setActivities] = useState({})
 
+  //Strava Credentials
+  const STRAVA_ID = process.env.NEXT_PUBLIC_CLIENT_ID
+  const STRAVA_SECRET = process.env.NEXT_PUBLIC_CLIENT_SECRET
+  const STRAVA_REFRESH_TOKEN = process.env.NEXT_PUBLIC_REFRESH_TOKEN
+
+  const callRefresh = `https://www.strava.com/oauth/token?client_id=${STRAVA_ID}&client_secret=${STRAVA_SECRET}&refresh_token=${STRAVA_REFRESH_TOKEN}&grant_type=refresh_token`
+
+  // endpoint for read-all activities. temporary token is added in getActivities()
+  const callActivities = `https://www.strava.com/api/v3/athlete/activities?access_token=`
+
+  // Use refresh token to get current access token
+  useEffect(() => {
+    fetch(callRefresh, {
+      method: 'POST'
+    })
+      .then((res) => res.json())
+      .then((result) => getActivities(result.access_token))
+  }, [callRefresh])
+
+  // use current access token to call all activities
+  function getActivities(access) {
+    fetch(callActivities + access)
+      .then((res) => res.json())
+      .then(
+        (data) => setActivities(data),
+        setIsLoading((prev) => !prev)
+      )
+      .catch((e) => console.log(e))
+  }
+
+  function showActivities() {
+    if (isLoading) return <>LOADING</>
+    if (!isLoading) {
+      const listActivities = Object.keys(activity).map((i) => activity[i])
+      return (
+        <S.ListActivities>
+          {listActivities.map((item, index) => {
+            const {
+              name,
+              distance,
+              moving_time,
+              type,
+              total_elevation_gain,
+              kudos_count,
+              suffer_score,
+              average_speed
+            } = item
+            return (
+              <li key={`activity-${index}`}>
+                <strong>{name}</strong>
+                <span>{type}</span>
+                <span>Distância: {distance}</span>
+                <span>Tempo:{moving_time}</span>
+                <span>Ganho de elev.{total_elevation_gain}</span>
+                <span>Kudos: {kudos_count}</span>
+                <span>Esforço relativo: {suffer_score}</span>
+                <span>Ritmo: {average_speed}</span>
+              </li>
+            )
+          })}
+        </S.ListActivities>
+      )
+    }
+  }
+  return <S.Wrapper>{showActivities()}</S.Wrapper>
+}
 export default Main
--- src/components/Main/styled.js
+++ src/components/Main/styled.js
@@ -1,41 +1,25 @@
 import styled from 'styled-components'
 
-const me =
-  'https://avatars.githubusercontent.com/u/2577611?s=460&u=118117a943294dfb5fb7e4050061133cf7476329&v=4'
-
-export const Wrapper = styled.main`
+export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
   background-color: var(--white);
   color: var(--black);
-  width: 100vw;
-  height: 100vh;
-  > div:hover {
-    background-size: 115%;
-  }
+  height: auto;
 `
 
-export const Banks = styled.div`
-  height: 30rem;
-  width: 30rem;
-  background-image: url(${me});
-  background-position: center;
-  background-size: 100%;
-  animation: morph 8s ease-in-out infinite;
-  border-radius: 60% 40% 30% 70% / 60% 30% 70% 40%;
-  box-shadow: 0 0.188rem 0.375rem var(--shadow);
-  transition: all 1s ease-in-out;
-  @keyframes morph {
-    0% {
-      border-radius: 60% 40% 30% 70% / 60% 30% 70% 40%;
-    }
-    50% {
-      border-radius: 30% 60% 70% 40% / 50% 60% 30% 60%;
-    }
-    100% {
-      border-radius: 60% 40% 30% 70% / 60% 30% 70% 40%;
+export const ListActivities = styled.ul`
+  display: flex;
+  flex-direction: column;
+
+  li {
+    display: flex;
+    gap: 20px;
+    list-style: none;
+    padding: 5px 0 5px 20px;
+    strong {
+      font-weight: 700;
+      margin-bottom: 20px;
     }
   }
 `
